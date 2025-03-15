6236bf7cfcfd635b1f67dd38cd48a77c9254dc65
DESKTOP-RDS99HT\user <wnsrl4067@gmail.com>
2025-03-15 19:13:31 +0900

    ### 오브젝트 풀 구현 완료 - TODO : 오브젝트 풀 링버퍼가 이용할 수 있도록 교체. (생성자 지원하게끔) - 직렬화 버퍼 구현 - 자동화의 가능성 열어두기

--- NetLib/ObjectPool.h
+++ NetLib/ObjectPool.h
@@ -1,1 +1,85 @@
 #pragma once
+
+namespace Common
+{
+	// objectpool 
+	template<typename T, size_t BucketCount = 256>
+	class ObjectPool
+	{
+	public:
+		typedef char _BYTE;
+		ObjectPool()
+			:_CurBucket(nullptr)
+			, _CurSlot(nullptr)
+			, _LastSlot(nullptr)
+			, _FreeList(nullptr)
+		{
+
+		}
+
+		~ObjectPool()
+		{
+			Slot* cur = _CurBucket;
+			while (cur != nullptr)
+			{
+				Slot* prev = cur->_pNext;
+				operator delete(reinterpret_cast<void*>(cur));
+				cur = prev;
+			}
+		}
+
+		T* allocate()
+		{
+			T* ret;
+			if (_FreeList != nullptr)
+			{
+				ret = &_CurSlot->_Data;
+				_FreeList = _FreeList->_pNext;
+				return ret;
+			}
+
+			//다 소모함 
+			if (_CurSlot >= _LastSlot)
+			{
+				allocateBucket();
+			}
+			ret = &_CurSlot->_Data;
+			_CurSlot++;
+			return ret;
+		}
+
+		void deAllocate(T* addr)
+		{
+			if (addr != nullptr)
+			{
+				Slot* retSlot = reinterpret_cast<Slot*>(reinterpret_cast<_BYTE*>(addr) - sizeof(Slot*));
+				retSlot->_pNext = _FreeList;
+				_FreeList = retSlot;
+				return;
+			}
+		}
+
+	private:
+		struct Slot
+		{
+			struct Slot* _pNext;
+			T _Data;
+		};
+
+		void allocateBucket()
+		{
+			Slot* newBucket = reinterpret_cast<Slot*>(operator new(sizeof(Slot) * (BucketCount + DUMMY_SIZE)));
+			newBucket->_pNext = _CurBucket;
+			_CurBucket = newBucket;
+			_CurSlot = _CurBucket + DUMMY_SIZE;
+			_LastSlot = newBucket + (BucketCount + DUMMY_SIZE);
+		}
+		enum {
+			DUMMY_SIZE = 1,
+		};
+		Slot* _CurBucket;
+		Slot* _CurSlot;
+		Slot* _LastSlot;
+		Slot* _FreeList;
+	};
+}
